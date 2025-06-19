44d73e5d43332df88585759d7100b1a157aae198
jgarea <jgntra@gmail.com>
2025-06-19 22:12:40 +0200

    vuex composition api setup

--- global-vuex/src/components/HelloWorld.vue
+++ global-vuex/src/components/HelloWorld.vue
@@ -4,26 +4,18 @@
 <button @click="increment">Increment</button>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent } from 'vue';
+<script lang="ts" setup>
+import { computed } from 'vue';
 import { useStore } from 'vuex';
-export default defineComponent({
-  name: 'HelloWorld',
-  setup() {
-      const store = useStore();
-      const counter = computed(() => store.state.counter);
-      const times2 = computed(() => store.getters.times2);
-      const increment = () => {
-        store.commit('setCounter', counter.value + 1);
-      };
-      return {
-        counter,
-        increment,
-        times2
-      };
-  }
+
+ 
+const store = useStore();
+const counter = computed(() => store.state.counter);
+const times2 = computed(() => store.getters.times2);
+const increment = () => {
+  store.commit('setCounter', counter.value + 1);
+};
   
-});
 </script>
 
 <style scoped>
