5714d015a77255e5b519fff93e9b6cbbde688421
jgarea <jgntra@gmail.com>
2025-07-11 18:07:49 +0200

    add watch option API

--- persistency/src/App.vue
+++ persistency/src/App.vue
@@ -1,33 +1,26 @@
 <template>
-  <h3>{{ store.count }}</h3>
-  <button @click="store.increment(1)">Añadir</button>
+ <h1>Notas:</h1>
+ <input type="text" v-model="note" >
 </template>
 
-<script lang="ts" setup>
-import { useExample } from '@/store/useExample';
-const store = useExample();
-store.init(); 
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      note: ''
+    };
+  },
+  watch: {
+    note(value, oldValue) {
+      alert(`Nota alterada de "${oldValue}" para "${value}"`);
+      localStorage.setItem('note', value);
+    }
+  }
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
